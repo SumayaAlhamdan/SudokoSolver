ca2948c18cca2edd54d9c44618bda90f885e8c13
Haldekhiel <109041734+Haldekhiel@users.noreply.github.com>
2024-03-31 22:32:00 +0300

    Add files via upload

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -1,109 +1,145 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class SudokuSolver {
-    private int[][] board;
-
-    public SudokuSolver(int[][] board) {
-        this.board = board;
+  
+  private static final int GRID_SIZE = 9;
+  
+  public static void main(String[] args) {
+    
+    int[][] board = {
+        {7, 0, 2, 0, 5, 0, 6, 0, 0},
+        {0, 0, 0, 0, 0, 3, 0, 0, 0},
+        {1, 0, 0, 0, 0, 9, 5, 0, 0},
+        {8, 0, 0, 0, 0, 0, 0, 9, 0},
+        {0, 4, 3, 0, 0, 0, 7, 5, 0},
+        {0, 9, 0, 0, 0, 0, 0, 0, 8},
+        {0, 0, 9, 7, 0, 0, 0, 0, 5},
+        {0, 0, 0, 2, 0, 0, 0, 0, 0},
+        {0, 0, 7, 0, 4, 0, 2, 0, 3} 
+    };
+    
+    printBoard(board);
+    
+    long startTime = System.currentTimeMillis();
+    
+    if (solveBoard(board)) {
+      long endTime = System.currentTimeMillis();
+      System.out.println("Solved successfully!");
+      System.out.println("Time taken: " + (endTime - startTime) + " milliseconds");
     }
-
-    public void solve() {
-        if (solveSudoku()) {
-            System.out.println("Sudoku puzzle solved:");
-            printBoard();
-        } else {
-            System.out.println("No solution exists.");
+    else {
+      System.out.println("Unsolvable board :(");
+    }
+    
+    printBoard(board);
+    
+  }
+  
+  
+  private static void printBoard(int[][] board) {
+    for (int row = 0; row < GRID_SIZE; row++) {
+      if (row % 3 == 0 && row != 0) {
+        System.out.println("-----------");
+      }
+      for (int column = 0; column < GRID_SIZE; column++) {
+        if (column % 3 == 0 && column != 0) {
+          System.out.print("|");
         }
+        System.out.print(board[row][column]);
+      }
+      System.out.println();
     }
+  }
 
-    private boolean solveSudoku() {
-        int[] emptyCell = findEmptyCell();
-        if (emptyCell == null) {
-            return true; // Puzzle solved successfully
+
+  private static boolean isNumberInRow(int[][] board, int number, int row) {
+    for (int i = 0; i < GRID_SIZE; i++) {
+      if (board[row][i] == number) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  private static boolean isNumberInColumn(int[][] board, int number, int column) {
+    for (int i = 0; i < GRID_SIZE; i++) {
+      if (board[i][column] == number) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  private static boolean isNumberInBox(int[][] board, int number, int row, int column) {
+    int localBoxRow = row - row % 3;
+    int localBoxColumn = column - column % 3;
+    
+    for (int i = localBoxRow; i < localBoxRow + 3; i++) {
+      for (int j = localBoxColumn; j < localBoxColumn + 3; j++) {
+        if (board[i][j] == number) {
+          return true;
         }
-
-        int row = emptyCell[0];
-        int col = emptyCell[1];
-
-        for (int num = 1; num <= 9; num++) {
-            if (isValidMove(row, col, num)) {
-                board[row][col] = num;
-
-                if (solveSudoku()) {
-                    return true;
-                }
-
-                board[row][col] = 0; // Backtrack
-            }
+      }
+    }
+    return false;
+  }
+  
+  private static boolean isValidPlacement(int[][] board, int number, int row, int column) {
+    return !isNumberInRow(board, number, row) &&
+        !isNumberInColumn(board, number, column) &&
+        !isNumberInBox(board, number, row, column);
+  }
+  
+  private static boolean solveBoard(int[][] board) {
+    int[] nextCell = findCellWithMRV(board);
+    if (nextCell == null) {
+        return true; // No empty cell found, board is solved
+    }
+    
+    int row = nextCell[0];
+    int column = nextCell[1];
+    
+    for (int numberToTry = 1; numberToTry <= GRID_SIZE; numberToTry++) {
+      if (isValidPlacement(board, numberToTry, row, column)) {
+        board[row][column] = numberToTry;
+        
+        if (solveBoard(board)) {
+          return true;
         }
-        return false;
-    }
-
-    private int[] findEmptyCell() {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] == 0) {
-                    return new int[]{i, j};
-                }
-            }
+        else {
+          board[row][column] = 0;
         }
-        return null; // No empty cell found
+      }
     }
-
-    private boolean isValidMove(int row, int col, int num) {
-        return !usedInRow(row, num) && !usedInColumn(col, num) && !usedInBox(row - row % 3, col - col % 3, num);
-    }
-
-    private boolean usedInRow(int row, int num) {
-        for (int i = 0; i < 9; i++) {
-            if (board[row][i] == num) {
-                return true;
-            }
+    return false;
+  }
+  
+  private static int[] findCellWithMRV(int[][] board) {
+    int[] minCell = null;
+    int minRemainingValues = Integer.MAX_VALUE;
+    
+    for (int row = 0; row < GRID_SIZE; row++) {
+      for (int column = 0; column < GRID_SIZE; column++) {
+        if (board[row][column] == 0) {
+        int remainingValues = countRemainingValues(board, row, column);
+          if (remainingValues < minRemainingValues) {
+            minRemainingValues = remainingValues;
+            minCell = new int[]{row, column};
+          }
         }
-        return false;
+      }
     }
-
-    private boolean usedInColumn(int col, int num) {
-        for (int i = 0; i < 9; i++) {
-            if (board[i][col] == num) {
-                return true;
-            }
-        }
-        return false;
+    return minCell;
+  }
+  
+  private static int countRemainingValues(int[][] board, int row, int column) {
+    Set<Integer> remainingValues = new HashSet<>();
+    for (int number = 1; number <= GRID_SIZE; number++) {
+      if (isValidPlacement(board, number, row, column)) {
+        remainingValues.add(number);
+      }
     }
-
-    private boolean usedInBox(int startRow, int startCol, int num) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board[startRow + i][startCol + j] == num) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    private void printBoard() {
-        for (int i = 0; i < 9; i++) {
-            System.out.println(Arrays.toString(board[i]));
-        }
-    }
-
-    public static void main(String[] args) {
-       int[][] puzzle = {
-    {5, 3, 0, 0, 7, 0, 0, 0, 0},
-    {6, 0, 0, 1, 9, 5, 0, 0, 0},
-    {0, 9, 8, 0, 0, 0, 0, 6, 0},
-    {8, 0, 0, 0, 6, 0, 0, 0, 3},
-    {4, 0, 0, 8, 0, 3, 0, 0, 1},
-    {7, 0, 0, 0, 2, 0, 0, 0, 6},
-    {0, 6, 0, 0, 0, 0, 2, 8, 0},
-    {0, 0, 0, 4, 1, 9, 0, 0, 5},
-    {0, 0, 0, 0, 8, 0, 0, 7, 9}
-};
-
-
-        SudokuSolver solver = new SudokuSolver(puzzle);
-        solver.solve();
-    }
+    return remainingValues.size();
+  }
+  
 }
